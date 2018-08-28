4a903fa79124052417bc6279f6da7b32c164652d
HisaCat <ahisacat@gmail.com>
2018-08-29 08:29:37 +0900

    fix eyetracking test

--- Assets/VRCSDKHelper/Scripts/Editor/EyeTrackingTest.cs
+++ Assets/VRCSDKHelper/Scripts/Editor/EyeTrackingTest.cs
@@ -17,13 +17,17 @@ namespace VRCSDKHelper
 
         private GameObject testEyeTracking = null;
         private Animator avatarAnimator;
-        float scaleSliderValue = 1;
+        float updownSliderValue = 0;
+        float leftRightSliderValue = 0;
+
+        private Transform leftEyeTrf = null;
+        private Transform rightEyeTrf = null;
 
         private void OnEnable()
         {
             EditorApplication.playmodeStateChanged += LogPlayModeState;
-            titleContent = new GUIContent("VRChat SDK Helper - Test EyeTracking");
-            minSize = maxSize = new Vector2(150, 150);
+            titleContent = new GUIContent("Test EyeTracking");
+            minSize = maxSize = new Vector2(150, 160);
             testEyeTracking = null;
 
             EditorApplication.isPlaying = true;
@@ -32,8 +36,12 @@ namespace VRCSDKHelper
         private void OnGUI()
         {
             GUILayout.BeginVertical();
+            GUILayout.Label("VRChat SDK Helper\r\nTest EyeTracking");
+            GUILayout.Space(4);
+
             if (Application.isEditor && Application.isPlaying)
             {
+
                 if (testEyeTracking == null)
                 {
                     testEyeTracking = GameObject.Instantiate(Resources.Load("TestEyeTrackingPrefab")) as GameObject;
@@ -42,14 +50,22 @@ namespace VRCSDKHelper
                     var headTrf = avatarAnimator.GetBoneTransform(HumanBodyBones.Head);
                     testEyeTracking.transform.position = headTrf.position - (headTrf.forward * -2f);
 
-                    var ep = testEyeTracking.GetComponentInChildren<EyeTrackingPoint>();
-                    ep.SetEyes(avatarAnimator.GetBoneTransform(HumanBodyBones.LeftEye), avatarAnimator.GetBoneTransform(HumanBodyBones.RightEye));
+                    leftEyeTrf = avatarAnimator.GetBoneTransform(HumanBodyBones.LeftEye);
+                    rightEyeTrf = avatarAnimator.GetBoneTransform(HumanBodyBones.RightEye);
                 }
 
-                GUILayout.Label("Scale");
-                scaleSliderValue = GUILayout.HorizontalSlider(scaleSliderValue, 0, 2);
-                testEyeTracking.GetComponentInChildren<EyeTrackingPoint>().SetScale(scaleSliderValue);
+                GUILayout.Label("Up - Down : " + (int)updownSliderValue);
+                updownSliderValue = GUILayout.HorizontalSlider(updownSliderValue, -19, 25);
+                GUILayout.Label("Left - Right : " + (int)leftRightSliderValue);
+                leftRightSliderValue = GUILayout.HorizontalSlider(leftRightSliderValue, -19, 19);
 
+                rightEyeTrf.localEulerAngles = leftEyeTrf.localEulerAngles = new Vector3(updownSliderValue, -leftRightSliderValue, 0);
+
+                if (GUILayout.Button("Reset to Default"))
+                {
+                    updownSliderValue = 0;
+                    leftRightSliderValue = 0;
+                }
                 if (GUILayout.Button("Stop"))
                 {
                     this.Close();
