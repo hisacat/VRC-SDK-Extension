b325a6f11a48c635ba52f56aa4fa780950d9f02f
HisaCat <ahisacat@gmail.com>
2018-09-03 20:45:30 +0900

    added GetSelectedProjectPath function (in editor)

---
+++ Assets/VRCSDKExtension/Scripts/Editor/Core/EditorExtension.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+namespace VRCSDKExtension
+{
+    public static class SelectionExtension
+    {
+        public static string GetSelectedProjectPath()
+        {
+            string path = "Assets";
+            foreach (UnityEngine.Object obj in Selection.GetFiltered(typeof(UnityEngine.Object), SelectionMode.Assets))
+            {
+                path = AssetDatabase.GetAssetPath(obj);
+                if (!string.IsNullOrEmpty(path) && File.Exists(path))
+                {
+                    path = Path.GetDirectoryName(path);
+                    break;
+                }
+            }
+            return path;
+        }
+    }
+}
